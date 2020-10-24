02bb12b6cfdb25ee7988c7a5f07ffc7c9982f898
Ondřej Švorc <52735242+ondrejsvorc@users.noreply.github.com>
2020-10-24 23:16:34 +0200

    Add files via upload

---
+++ Slozky.ps1
@@ -0,0 +1,14 @@
+[string] $folderName = "Kasiopea " + (Get-Date).Year.ToString()                         
+[string] $path = "C:\Users\" + [Environment]::UserName + "\Desktop\" + $folderName      
+
+if (!(Test-Path $path))                                                    
+{
+    New-Item -ItemType Directory -Path $path          
+    Set-Location -Path $path                          
+
+    for ($i = 1; $i -lt 11; $i++) 
+    {
+        New-Item -ItemType Directory -Path .\$i
+        New-Item -ItemType File -Path .\$i\out.txt
+    }
+}
