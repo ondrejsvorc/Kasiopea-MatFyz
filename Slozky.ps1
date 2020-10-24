d6d033379c609c5b2dd186e54540c3aabbea7f92
Ondřej Švorc <52735242+ondrejsvorc@users.noreply.github.com>
2020-10-24 23:51:53 +0200

    Update Slozky.ps1

--- Slozky.ps1
+++ Slozky.ps1
@@ -6,7 +6,7 @@ if (!(Test-Path $path))
     New-Item -ItemType Directory -Path $path          
     Set-Location -Path $path                          
 
-    for ($i = 1; $i -lt 11; $i++) 
+    for ($i = 1; $i -lt 9; $i++) 
     {
         New-Item -ItemType Directory -Path .\$i
         New-Item -ItemType File -Path .\$i\out.txt
