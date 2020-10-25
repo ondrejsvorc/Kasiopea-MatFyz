498238b6019a5939da3202bf70b73966b5b1e9e2
Ondřej Švorc <52735242+ondrejsvorc@users.noreply.github.com>
2020-10-25 11:01:10 +0100

    Update Slozky.ps1

--- Slozky.ps1
+++ Slozky.ps1
@@ -1,14 +1,35 @@
 [string] $folderName = "Kasiopea " + (Get-Date).Year.ToString()                         
-[string] $path = "C:\Users\" + [Environment]::UserName + "\Desktop\" + $folderName      
+[string] $path = "C:\Users\" + [Environment]::UserName + "\Desktop\" + $folderName
+[string] $firstRound = "Domaci kolo"
+[string] $finalRound = "Finale"      
+[int] $subFolderASCII = 65
 
 if (!(Test-Path $path))                                                    
 {
-    New-Item -ItemType Directory -Path $path          
-    Set-Location -Path $path                          
-
-    for ($i = 1; $i -lt 9; $i++) 
+    New-Item -ItemType Directory -Path $path
+    Set-Location -Path $path
+    New-Item -ItemType Directory -Path .\$firstRound
+    Set-Location -Path $path\$firstRound           
+                          
+    for ($i = 0; $i -lt 8; $i++) 
     {
-        New-Item -ItemType Directory -Path .\$i
-        New-Item -ItemType File -Path .\$i\out.txt
+        [char] $subFolderName = [char] $subFolderASCII++
+
+        New-Item -ItemType Directory -Path .\$subFolderName
+        New-Item -ItemType File -Path .\$subFolderName\out.txt
+    }
+    
+    $subFolderASCII = 65
+
+    Set-Location -Path $path
+    New-Item -ItemType Directory -Path .\$finalRound
+    Set-Location -Path $path\$finalRound
+
+    for ($i = 0; $i -lt 6; $i++) 
+    {
+        [char] $subFolderName = [char] $subFolderASCII++
+
+        New-Item -ItemType Directory -Path .\$subFolderName
+        New-Item -ItemType File -Path .\$subFolderName\out.txt
     }
 }
